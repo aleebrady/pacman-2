1c3fea3d51e9681e359b3fdf86879caa81fda772
aleebrady <aleebrady@gmail.com>
2021-03-25 11:59:24 -0700

    setup functions needed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('Working!');
+import {LEVEL, OBJECT_TYPE } from './setup'
+
+// DOM Elements
+const gameGrid = document.querySelector('#game')
+const scoreTable = document.querySelector('#score')
+const startButton = document.querySelector('#start-button')
+
+// Game constants - ALL CAPS: THEY CAN'T CHANGE IN THE GAME
+const POWER_PILL_TIME = 10000 //ms
+const GLOBAL_SPEED = 80 // ms
+
+// initial setup
+let score = 0
+let timer = null 
+let gameWin = false
+let powerPillActive = false
+let powerPillTimer = null
+
+function gameOver(pacman, grid) {
+
+}
+
+function checkCollision(pacman, ghosts) {
+
+}
+
+function gameLoop(pacman, ghosts) {
+
+}
+
+function startGame() {
+    
+}
